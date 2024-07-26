45b57f0cd32519a40412e6aebadef7388f8ba4c1
Pranjal Bhagat <117507692+Pranjal-2004@users.noreply.github.com>
2024-07-26 23:57:39 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -53,7 +53,7 @@ export default function SignIn() {
             </span>
           </Link>
           <p className='text-sm mt-5'>
-            This website is a task given by vercos
+            This website is a blogginf website
           </p>
         </div>
         {/* right */}
